cba36464c26aee9d173f21bcf8e8e2950e7995c0
Nandana <nandanasheri@gmail.com>
2024-05-25 11:15:28 -0500

    shifted env, added axios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-const express = require('express');
-const {google} = require('googleapis');
-const process = require('process');
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import { google } from 'googleapis';
 
 const app = express();
 const PORT = process.env.NODE_ENV || 8000;
 
-const auth2Client  = new google.auth.OAuth2(
+const oauth2Client  = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
     process.env.REDIRECT_URL
 )
 
+// generate a url that asks permissions for Blogger and Google Calendar scopes
+const scopes = [
+    'https://www.googleapis.com/auth/calendar'
+  ];
+
 app.get('/google', (req, res) => {
-    
-})
+    const url = oauth2Client.generateAuthUrl({
+        access_type : 'offline',
+        scope: scopes,
+    });
+    res.redirect(url)
+});
+
+app.get('/google/redirect', (req, res) => {
+    const token = req.query.code;
+    res.send("it's working");
+});
 
 app.listen(PORT, () => {
     console.log("Server started on port", PORT);
-})
+});
