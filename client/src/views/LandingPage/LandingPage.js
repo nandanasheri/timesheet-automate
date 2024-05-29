95d1764e5a2b2b3515e6f9eae11dc7502464bad7
Nandana <nandanasheri@gmail.com>
2024-05-28 19:14:50 -0500

    landing page title - panda needs alignment

--- client/src/views/LandingPage/LandingPage.js
+++ client/src/views/LandingPage/LandingPage.js
@@ -1,9 +1,26 @@
 import styles from "./LandingPage.module.css";
+import timesheetlogo from "../../assets/timesheet-cropped.svg";
+import samplepdf from "../../assets/form-sample.svg";
+import pandapdf from "../../assets/panda-on-pdf.svg";
 
 function LandingPage() {
     return (
         <div>
-            this is landing page
+            <div className={styles["landing-header"]}>
+                <img src={timesheetlogo} alt="Logo" />
+            </div>
+            <div className={styles["landing-title-container"]}>
+                <div className={styles["landing-title-item"]}>
+                    <h1 className={styles["landing-title"]}>Auto fill your time sheets on the click of a button</h1>
+                    <h4 className={styles["landing-title-subtext"]}>With Timesheet Automate, you can generate a filled timesheet solely by having your work hours on Google Calendar</h4>
+                </div>
+                <div className={styles["landing-title-right"]}>
+                    <img src={pandapdf} alt="Panda on PDF" />
+                    <div className={styles["landing-title-pdf-container"]}>
+                        <img src={samplepdf} alt="Sample PDF" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
