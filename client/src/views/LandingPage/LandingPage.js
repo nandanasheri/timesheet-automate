2f4c45b41d59c951d099e8db6cfa764a7d02a97c
Nandana <nandanasheri@gmail.com>
2024-06-03 22:37:34 -0500

    main landing added

--- client/src/views/LandingPage/LandingPage.js
+++ client/src/views/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import styles from "./LandingPage.module.css";
 import timesheetlogo from "../../assets/timesheet-cropped.svg";
 import samplepdf from "../../assets/form-sample.svg";
 import pandapdf from "../../assets/panda-on-pdf.svg";
+import arrow from "../../assets/arrow.svg";
 
 function LandingPage() {
     return (
@@ -13,9 +14,17 @@ function LandingPage() {
                 <div className={styles["landing-title-item"]}>
                     <h1 className={styles["landing-title"]}>Auto fill your time sheets on the click of a button</h1>
                     <h4 className={styles["landing-title-subtext"]}>With Timesheet Automate, you can generate a filled timesheet solely by having your work hours on Google Calendar</h4>
+                    <div className={styles["landing-login-button"]}>
+                        <button type="button">LOG IN</button>
+                    </div>
                 </div>
+
+                <div>
+                    <img className={styles["landing-arrow"]} src={arrow} alt="Panda on PDF" />
+                </div>
+
                 <div className={styles["landing-title-right"]}>
-                    {/* <img className={styles["landing-panda"]} src={pandapdf} alt="Panda on PDF" /> */}
+                    <img className={styles["landing-panda"]} src={pandapdf} alt="Panda on PDF" />
                     <div className={styles["landing-title-pdf-container"]}>
                         <img className={styles["landing-title-pdf-form"]} src={samplepdf} alt="Sample PDF" />
                     </div>
