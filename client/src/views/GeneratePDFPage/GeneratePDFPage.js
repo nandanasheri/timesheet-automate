60207e576f8cad3550c46702557353dfd27d7a85
Nandana <nandanasheri@gmail.com>
2024-06-04 16:11:07 -0500

    started working on generate pdf

--- client/src/views/GeneratePDFPage/GeneratePDFPage.js
+++ client/src/views/GeneratePDFPage/GeneratePDFPage.js
@@ -1,9 +1,18 @@
 import styles from "../GeneratePDFPage/GeneratePDFPage.module.css";
+import timesheetlogo from "../../assets/timesheet-cropped.svg";
+import sadpanda from "../../assets/sadpanda.svg";
 
 function GeneratePDFPage () {
     return (
-        <div>
-            
+        <div className={styles["generate-main"]}>
+            <div>
+                <img className={styles["generate-logo"]} src={timesheetlogo} alt="Logo" />
+            </div>
+            <div className={styles["generate-form-title"]}>
+                <div className={styles["generate-circle"]}>01</div>
+                <h2>Fill out the following details:</h2>
+                <img className={styles["generate-panda"]} src={sadpanda}></img>
+            </div>
         </div>
     )
 };
