2b8fc25dff170607b503d8ec4021155e3f3556c8
Nandana <nandanasheri@gmail.com>
2024-06-05 00:30:30 -0500

    added titles for generate - todo form

--- client/src/views/GeneratePDFPage/GeneratePDFPage.js
+++ client/src/views/GeneratePDFPage/GeneratePDFPage.js
@@ -1,6 +1,8 @@
 import styles from "../GeneratePDFPage/GeneratePDFPage.module.css";
 import timesheetlogo from "../../assets/timesheet-cropped.svg";
 import sadpanda from "../../assets/sadpanda.svg";
+import footerpanda from "../../assets/footer panda.svg";
+
 
 function GeneratePDFPage () {
     return (
@@ -11,8 +13,30 @@ function GeneratePDFPage () {
             <div className={styles["generate-form-title"]}>
                 <div className={styles["generate-circle"]}>01</div>
                 <h2>Fill out the following details:</h2>
-                <img className={styles["generate-panda"]} src={sadpanda}></img>
+                <img className={styles["generate-panda"]} src={sadpanda} alt="sadpanda"></img>
             </div>
+
+            <div className={styles["generate-form-title"]}>
+                <div className={styles["generate-circle"]}>02</div>
+                <h2>Generate PDF:</h2>
+            </div>
+
+            <div className={styles["generate-form-title"]}>
+                <div className={styles["generate-circle"]}>03</div>
+                <h2>Check your Downloads!</h2>
+            </div>
+            <div className={styles["generate-thankyou"]}>
+                <h2>Thank you for using Timesheet Automate!</h2>
+            </div>
+            
+            <div className={styles["generate-footer-container"]}>
+                <div style={{display:"flex", alignItems:"center"}}>
+                    <h3 className={styles["generate-footer"]}>Made with &lt;3</h3>
+                    <img src={footerpanda} alt="footerpanda" />
+                </div>
+                <h4 className={styles["generate-footer"]}> Built by Nandana Sheri</h4>
+            </div>
+
         </div>
     )
 };
